91bc47e56078715fdb1718cc0943c407ffa0d235
hari-hk <hariharakumar5196@gmail.com>
2020-07-30 21:49:33 +0530

    added extra fields

--- src/app/register-hotel/register-hotel.component.ts
+++ src/app/register-hotel/register-hotel.component.ts
@@ -12,17 +12,13 @@ export class RegisterHotelComponent implements OnInit {
   registerForm: FormGroup;
   submitted = false;
   isChecked = [];
-  optionList = [
-    { name: 'Wifi', id: 1 },
-    { name: 'Free Car parking', id: 2 },
-    { name: 'Projector', id: 3 },
-    { name: 'TV', id: 4 },
-    { name: 'Beverages', id: 5 },
-  ];
+  optionList = [];
   optionsSelected = [];
-  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
-  constructor(private fb: FormBuilder, private dialogService: DialogService,
-    private apiService: PostDataService, private el: ElementRef) { }
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$';
+  constructor(private fb: FormBuilder,
+    private dialogService: DialogService,
+    private apiService: PostDataService,
+    private el: ElementRef) { }
 
   ngOnInit(): void {
     this.getInitialData();
@@ -42,82 +38,66 @@ export class RegisterHotelComponent implements OnInit {
       street: ['', Validators.required],
       city: ['', Validators.required],
       pinCode: ['', Validators.required],
-      offpeak: ['', Validators.required],
-      peak: ['', Validators.required],
-      xmas: ['', Validators.required],
+      minOffPeak: ['', Validators.required],
+      minHrPeak: ['', Validators.required],
+      minHrXmas: ['', Validators.required],
       remarks: [''],
       privateArea: this.fb.array([])
-    })
+    });
   }
   get privateAreas(): FormArray {
-    return this.registerForm.get("privateArea") as FormArray;
+    return this.registerForm.get('privateArea') as FormArray;
   }
   createPrivateAreas(): FormGroup {
     return this.fb.group({
       areaName: ['', Validators.required],
-      numberOfRoom: ['', Validators.required],
-      numberOfSeats: ['', Validators.required]
-    })
+      noOfRoom: ['', Validators.required],
+      noOfSeats: ['', Validators.required]
+    });
   }
   getInitialData() {
     const params = {
       limit: 10,
       offset: 0
-    }
-    this.apiService.get('option/list', params).subscribe(res => {
-
-    })
+    };
+    this.apiService.get('option/list', params).subscribe((res: any) => {
+      this.optionList = res.data.optionList;
+    });
   }
   addPrivateAreas() {
-    this.privateAreas.push(this.createPrivateAreas())
+    this.privateAreas.push(this.createPrivateAreas());
   }
   removePrivateAreas(i: number) {
     this.privateAreas.removeAt(i);
   }
   onSelectOption(event, item, i): void {
     if (event.target.checked) {
-      this.optionsSelected.push(item.id)
+      this.optionsSelected.push(item.optionId);
       this.isChecked[i] = true;
     } else {
       this.isChecked[i] = false;
-      this.optionsSelected = this.optionsSelected.filter(x => {
-        if (x.id == item.id) {
-          return false;
-        } else {
-          return true;
-        }
-      })
+      this.optionsSelected = this.optionsSelected.filter(el => el.optionId !== item.optionId);
     }
   }
   onSubmit() {
     this.submitted = true;
-    console.log('submitted', this.registerForm)
+    console.log('submitted', this.registerForm);
     if (!this.registerForm.valid) {
       this.setfocus();
       return;
     }
     const data = this.registerForm.value;
-    delete data.offpeak;
-    delete data.peak;
-    delete data.xmas;
-    data.privateArea = '';
     data.options = this.optionsSelected.join(',');
-    // this.optionsSelected.forEach((x, i) => {
-    //   data.options += x;
-    //   if (i < this.optionsSelected.length -1) {
-    //     data.options += ',';
-    //   }
-    // })
     this.apiService.post('user/register', data).subscribe((res: any) => {
       if (res.status == 1) {
-        this.dialogService.show.next({ message: "Registered Successfully", title: "Success", status: true });
+        this.dialogService.show.next({ message: 'Registered Successfully', title: 'Success', status: true });
         this.registerForm.reset();
         this.registerForm.clearValidators();
         this.submitted = false;
         this.isChecked = [];
-        window.scrollTo(0, 0)
+        window.scrollTo(0, 0);
       } else {
-        this.dialogService.show.next({ message: "Conact Your Administaror", title: "Error", status: true });
+        this.dialogService.show.next({ message: 'Conact Your Administaror', title: 'Error', status: true });
       }
     });
   }
